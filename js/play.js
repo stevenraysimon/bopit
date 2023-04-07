69a2c4727f10997760f4b792e1cd17f1ce826e00
Steven Simon <stevensimon@Stevens-iMac.local>
2023-04-07 02:23:31 -0500

    Keyboard lighting added

--- js/play.js
+++ js/play.js
@@ -14,4 +14,63 @@ document.addEventListener("DOMContentLoaded", () => {
             playing = false;
         }
     });//click playPause
+
+    //Set up ids
+    function setIds(){
+        var keys = document.getElementsByClassName("key");
+        for (var i = 0; i < keys.length; i++) {
+            if(keys.item(i).getAttribute('id') === null){
+                if(keys.item(i).querySelectorAll('div')[1]){
+                    keys.item(i).setAttribute('id', keys.item(i).querySelectorAll('div')[1].innerHTML);
+                } else if(keys.item(i).querySelectorAll('div')[0]){
+                    keys.item(i).setAttribute('id', keys.item(i).querySelectorAll('div')[0].innerHTML);
+                } else{
+                    keys.item(i).setAttribute('id', keys.item(i).innerHTML);
+                }
+            }
+        }
+    }
+    setIds();
+
+    //Keyboard events
+    document.addEventListener('keypress', (event) => {
+        var name = event.key;
+        var code = event.code;
+
+        //Catch input
+        document.getElementById('input').value += name;
+
+        //Make string
+        name = String(name).toUpperCase();
+
+        //Keyboard Lights
+        document.getElementById(`${name}`).classList.add('keyPressed');
+
+        setTimeout(function(){
+            document.getElementById(`${name}`).classList.remove('keyPressed');
+        }, 100);
+
+
+        //alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+    }, false);
+
+    document.addEventListener("keydown", function(event){
+        const key = event.key;
+        var getInput = document.getElementById('input').value;
+        if (key === "Backspace") {
+            document.getElementById('backspace').classList.add('keyPressed');
+            document.getElementById('input').value = getInput.substring(0, getInput.length - 1);
+            setTimeout(function(){
+                document.getElementById('backspace').classList.remove('keyPressed');
+            }, 100);
+            return false;
+        }
+    
+    });
+
+    // document.addEventListener("keyup", function(){
+    //     document.getElementById(`${name}`).style.backgroundColor = '#2283FF';
+    //     document.getElementById(`${name}`).style.color = '#FFF';
+    // });//Refresh keys
+
 });//doc ready
