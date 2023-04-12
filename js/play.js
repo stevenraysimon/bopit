44bea40e5014ef28cb0c2bb3d29c15dd905846d9
Steven Simon <ssimon@Stevens-MacBook-Pro.local>
2023-04-11 19:35:29 -0500

    Fixed caps lock

--- js/play.js
+++ js/play.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    //Variables
+    var caps = false;
+
+    //Play and pause
     document.querySelector('.playPause').setAttribute('id', 'playPause');
     let playing = false;
     document.querySelector('.playPause').addEventListener('click', function(){
@@ -46,17 +51,20 @@ document.addEventListener("DOMContentLoaded", () => {
         //Keyboard Lights
         document.getElementById(`${name}`).classList.add('keyPressed');
 
-        setTimeout(function(){
-            document.getElementById(`${name}`).classList.remove('keyPressed');
-        }, 100);
-
+        if (name != 'capslock'){
+            setTimeout(function(){
+                document.getElementById(`${name}`).classList.remove('keyPressed');
+            }, 100);
+        }
 
         //alert(`Key pressed ${name} \r\n Key code value: ${code}`);
     }, false);
 
+    //Backspace
     document.addEventListener("keydown", function(event){
         const key = event.key;
         var getInput = document.getElementById('input').value;
+
         if (key === "Backspace") {
             document.getElementById('backspace').classList.add('keyPressed');
             document.getElementById('input').value = getInput.substring(0, getInput.length - 1);
@@ -65,8 +73,53 @@ document.addEventListener("DOMContentLoaded", () => {
             }, 100);
             return false;
         }
+
+
+        //If caps locks
+        // if(key === "CapsLock"){
+        //     checkCaps();
+        // } 
     
     });
+
+    function checkCaps(){
+        if(caps === true){
+            document.getElementById('capslock').style.backgroundColor = '#e9e9e9';
+            caps = false;
+        }else{
+            document.getElementById('capslock').style.backgroundColor = '#38fd95';
+            caps = true;
+        }
+    }
+
+    //Keyboard click
+    var screenKeys = document.getElementsByClassName('key');
+    for (var i = 0; i < screenKeys.length; i++) {
+        screenKeys[i].addEventListener('click', keyLight, false);
+    }
+    function keyLight(){
+        var input = this.id;
+
+        if(input == 'capslock'){
+            input = '';
+            checkCaps();
+        }
+
+        //Catch caps lock
+        if(caps == true){
+            input = input.toUpperCase();
+        } else{
+            input = input.toLowerCase();
+        }
+
+        //Catch input
+        if(input == 'backspace'){
+            var getInput = document.getElementById('input').value;
+            document.getElementById('input').value = getInput.substring(0, getInput.length - 1);
+        } else{
+            document.getElementById('input').value += input; 
+        }
+    }
 
     // document.addEventListener("keyup", function(){
     //     document.getElementById(`${name}`).style.backgroundColor = '#2283FF';
